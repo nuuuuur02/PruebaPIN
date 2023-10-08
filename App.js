3ce22aef1690febcc0e024335f024def2a566b32
nuuuuur02 <84862968+nuuuuur02@users.noreply.github.com>
2023-10-08 11:49:05 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import ScreenLogin from './screens/ScreenLogin';
+import ScreenHome from './screens/ScreenHome';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen options ={ {headerShown: false} } name="Login" component={ScreenLogin} />
+        <Stack.Screen name="Home" component={ScreenHome} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ firebase.js
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyAaUH_Or8hVoEf-d3jtozQUVkfVLiA-qOU",
+  authDomain: "fir-pinlogin.firebaseapp.com",
+  projectId: "fir-pinlogin",
+  storageBucket: "fir-pinlogin.appspot.com",
+  messagingSenderId: "823878648840",
+  appId: "1:823878648840:web:79a7d447ee1ffc11fc7dbd"
+};
+
